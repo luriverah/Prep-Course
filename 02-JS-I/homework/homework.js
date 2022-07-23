732445f5bfbd37919420a0591d67f57c55740549
Miguel Rivera <luislmrh02@hotmail.com>
2022-07-23 17:38:13 -0500

    fuourth commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -255,7 +255,7 @@ function esVocal(letra){
   if (letra.length != 1){
     return 'Dato incorrecto'
   }
-  if (letra == 'a' || 'e' || 'i' || 'o' || 'u' ){
+  if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ){
     return 'Es vocal'
   }
   return 'Dato incorrecto'
